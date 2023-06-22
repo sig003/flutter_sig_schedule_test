2f52425c48df965a90737df5d6ac140614980e7c
sig03 <sig03@naver.com>
2023-06-23 08:28:17 +0900

    add listview

--- lib/screens/schedule_list.dart
+++ lib/screens/schedule_list.dart
@@ -8,19 +8,38 @@ class ScheduleList extends StatefulWidget {
 }
 
 class _ScheduleListState extends State<ScheduleList> {
-  final List<String> entries = <String>['A', 'B', 'C'];
-  final List<int> colorCodes = <int>[600, 500, 100];
+  final List<Map<String, dynamic>> contents = [
+    {'title': '약먹기', 'time': '2023-06-10 12:00:00'},
+    {'title': '쇼핑', 'time': '2023-06-10 12:00:00'},
+    {'title': '티비', 'time': '2023-06-10 12:00:00'}
+  ];
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-        padding: const EdgeInsets.all(8),
-        itemCount: entries.length,
+        padding: const EdgeInsets.all(18),
+        itemCount: contents.length,
         itemBuilder: (BuildContext context, int index) {
-          return Container(
-            height: 50,
-            color: Colors.amber[colorCodes[index]],
-            child: Center(child: Text('Entry ${entries[index]}')),
+          return Column(
+            children: [
+              SizedBox(height: 10,),
+              Container(
+                height: 80,
+                child: Card(
+                  child: Container(
+                    //height: 100,
+                    child: Center(
+                      child: Column(
+                        children: [
+                          Text(contents[index]['title']),
+                          Text(contents[index]['time']),
+                        ],
+                      ),
+                    )
+                  ),
+                ),
+              ),
+            ],
           );
         }
     );
