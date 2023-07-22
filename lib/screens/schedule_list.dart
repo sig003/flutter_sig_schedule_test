7084ae02d2799385fd8f9feaa7322f1986af3076
sig03 <sig03@sig03ui-MacBookPro.local>
2023-07-22 10:28:15 +0900

    fix listing

--- lib/screens/schedule_list.dart
+++ lib/screens/schedule_list.dart
@@ -27,93 +27,6 @@ class Job {
 
 class _ScheduleListState extends State<ScheduleList> {
   var ListArray = [];
-  // final List<Map<String, dynamic>> contents = [
-  //   {'title': '약먹기', 'date': '2023-06-10 12:00:00'},
-  //   {'title': '쇼핑', 'date': '2023-06-10 12:00:00'},
-  //   {'title': '티비', 'date': '2023-06-10 12:00:00'}
-  // ];
-  //
-  // var count = 0;
-  //
-  // @override
-  // void initState() {
-  //   // TODO: implement initState
-  //   super.initState();
-  //   _getData();
-  // }
-
-  // @override
-  // Widget build(BuildContext context) {
-  //   return ListView.builder(
-  //       padding: const EdgeInsets.all(18),
-  //       itemCount: count,
-  //       itemBuilder: (BuildContext context, int index) {
-  //         return Column(
-  //           children: [
-  //             SizedBox(height: 10,),
-  //             Container(
-  //               height: 80,
-  //               child: Card(
-  //                 child: Container(
-  //                   //height: 100,
-  //                   child: Center(
-  //                     child: Row(
-  //                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-  //                       children: [
-  //                         Container(
-  //                           padding: EdgeInsets.only(left: 10),
-  //                           child: Column(
-  //                             mainAxisAlignment: MainAxisAlignment.center,
-  //                             children: [
-  //                               // Text(contents[index]['title']),
-  //                               // SizedBox(height: 10,),
-  //                               // Text(contents[index]['date']),
-  //                               FutureBuilder(
-  //                                   future: _getData(),
-  //                                   builder: (BuildContext context, AsyncSnapshot snapshot) {
-  //                                     if (snapshot.hasData == false) {
-  //                                       return CircularProgressIndicator();
-  //                                     }
-  //                                     else if (snapshot.hasError) {
-  //                                       return Padding(
-  //                                         padding: const EdgeInsets.all(8.0),
-  //                                         child: Text(
-  //                                           'Error: ${snapshot.error}',
-  //                                           style: TextStyle(fontSize: 15),
-  //                                         ),
-  //                                       );
-  //                                     }
-  //                                     else {
-  //                                       return Padding(
-  //                                         padding: const EdgeInsets.all(8.0),
-  //                                         child: Text(
-  //                                           snapshot.data.toString(),
-  //                                           style: TextStyle(fontSize: 15),
-  //                                         ),
-  //                                       );
-  //                                     }
-  //                                   })
-  //                             ],
-  //                           ),
-  //                         ),
-  //                         Container(
-  //                           padding: EdgeInsets.only(right: 10),
-  //                           child: Icon(
-  //                             Icons.delete,
-  //                             color: Colors.red,
-  //                           ),
-  //                         )
-  //                       ],
-  //                     ),
-  //                   )
-  //                 ),
-  //               ),
-  //             ),
-  //           ],
-  //         );
-  //       }
-  //   );
-  // }
 
   @override
   Widget build(BuildContext context) {
@@ -138,9 +51,26 @@ class _ScheduleListState extends State<ScheduleList> {
                                     child: Column(
                                       mainAxisAlignment: MainAxisAlignment.center,
                                       children: [
-                                        Padding(
-                                          padding: const EdgeInsets.all(8.0),
-                                          child: Text(snapshot.data?[index]['job'] ?? ''),
+                                        Column(
+                                          crossAxisAlignment: CrossAxisAlignment.start,
+                                          children: [
+                                            Padding(
+                                              padding: const EdgeInsets.fromLTRB(8, 5, 0, 5),
+                                              child: Text(snapshot.data?[index]['job'], style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold)),
+                                            ),
+                                            Row(
+                                              children: [
+                                                Padding(
+                                                  padding: const EdgeInsets.all(8.0),
+                                                  child: Text(snapshot.data?[index]['date']),
+                                                ),
+                                                Padding(
+                                                  padding: const EdgeInsets.all(8.0),
+                                                  child: Text(snapshot.data?[index]['time']),
+                                                ),
+                                              ],
+                                            ),
+                                          ],
                                         ),
                                       ],
                                     )
@@ -167,20 +97,16 @@ class _ScheduleListState extends State<ScheduleList> {
 
   Future<List<dynamic>> _getData() async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
-    //prefs.clear();
+    List<dynamic> jsonData = prefs.getStringList('data') ?? [];
+
+    ListArray = [];
+    for (int i = 0; i < jsonData.length; i++) {
+      ListArray.add(jsonDecode(jsonData[i]));
+    }
+
     // setState(() {
     //
     // });
-    List<dynamic> bb = prefs.getStringList('data') ?? [];
-
-    for (int i = 0; i < bb.length; i++) {
-      ListArray.add(jsonDecode(bb[i]));
-    }
-
-    // List<Map<String, String>> aa = [{"id":"c6b63d40-2289-11ee-a39b-9b519e963c49","job":"abcd","date":"2023-07-15","time":"06:03"}];
-    // print(aa);
-
-    print(ListArray);
     return ListArray;
   }
 }
