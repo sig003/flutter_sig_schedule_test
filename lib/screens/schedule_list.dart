8b4e3aad5c7910ff4657ab6411530ab16947b4a3
sig03 <sig03@naver.com>
2023-07-27 07:48:17 +0900

    modify listing

--- lib/screens/schedule_list.dart
+++ lib/screens/schedule_list.dart
@@ -33,92 +33,91 @@ class _ScheduleListState extends State<ScheduleList> {
     return FutureBuilder(
       future: _getData(),
       builder: (context, snapshot) {
-        var aa = snapshot.data?.length ?? 0;
-        if (aa > 0) {
-          return Text('Has Data');
-        }
-        return Text('No data');
-          // if (snapshot.hasData) {
-          //   return ListView.builder(
-          //     itemCount: snapshot.data?.length,
-          //     itemBuilder: (BuildContext context, int index) {
-          //         List<dynamic>? resultData = snapshot.data;
-          //           return Column(
-          //               children: [
-          //                 SizedBox(height: 10,),
-          //                 Container(
-          //                   height: 80,
-          //                   child: Card(
-          //                     child: Container(
-          //                         child: Center(
-          //                             child: Row(
-          //                               mainAxisAlignment: MainAxisAlignment
-          //                                   .spaceBetween,
-          //                               children: [
-          //                                 Container(
-          //                                     padding: EdgeInsets.only(left: 10),
-          //                                     child: Column(
-          //                                       mainAxisAlignment: MainAxisAlignment
-          //                                           .center,
-          //                                       children: [
-          //                                         Column(
-          //                                           crossAxisAlignment: CrossAxisAlignment
-          //                                               .start,
-          //                                           children: [
-          //                                             Padding(
-          //                                               padding: const EdgeInsets
-          //                                                   .fromLTRB(8, 5, 0, 5),
-          //                                               child: Text(
-          //                                                   resultData?[index]['job'] ??
-          //                                                       'None',
-          //                                                   style: TextStyle(
-          //                                                       fontSize: 17,
-          //                                                       fontWeight: FontWeight
-          //                                                           .bold)),
-          //                                             ),
-          //                                             Row(
-          //                                               children: [
-          //                                                 Padding(
-          //                                                   padding: const EdgeInsets
-          //                                                       .all(8.0),
-          //                                                   child: Text(
-          //                                                       resultData?[index]['date'] ??
-          //                                                           'None'),
-          //                                                 ),
-          //                                                 Padding(
-          //                                                   padding: const EdgeInsets
-          //                                                       .all(8.0),
-          //                                                   child: Text(
-          //                                                       resultData?[index]['time'] ??
-          //                                                           'None'),
-          //                                                 ),
-          //                                               ],
-          //                                             ),
-          //                                           ],
-          //                                         ),
-          //                                       ],
-          //                                     )
-          //                                 ),
-          //                                 Container(
-          //                                   padding: EdgeInsets.only(right: 10),
-          //                                   child: Icon(
-          //                                     Icons.delete,
-          //                                     color: Colors.red,
-          //                                   ),
-          //                                 ),
-          //                               ],
-          //                             )
-          //                         )
-          //                     ),
-          //                   ),
-          //                 ),
-          //               ]
-          //           );
-          //       }
-          //     );
-          //   } else {
-          //   return Text('No Data');
-          // }
+        int dataLength = snapshot.data?.length ?? 0;
+        if (dataLength <= 0) {
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Center(child: Text('No Data')),
+            ],
+          );
+        } else {
+          return ListView.builder(
+            itemCount: dataLength,
+            itemBuilder: (BuildContext context, int index) {
+                List<dynamic>? resultData = snapshot.data;
+                  return Column(
+                      children: [
+                        SizedBox(height: 10,),
+                        Container(
+                          height: 80,
+                          child: Card(
+                            child: Container(
+                                child: Center(
+                                    child: Row(
+                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Container(
+                                            padding: EdgeInsets.only(left: 10),
+                                            child: Column(
+                                              mainAxisAlignment: MainAxisAlignment.center,
+                                              children: [
+                                                Column(
+                                                  crossAxisAlignment: CrossAxisAlignment
+                                                      .start,
+                                                  children: [
+                                                    Padding(
+                                                      padding: const EdgeInsets
+                                                          .fromLTRB(8, 5, 0, 5),
+                                                      child: Text(
+                                                          resultData?[index]['job'] ??
+                                                              'None',
+                                                          style: TextStyle(
+                                                              fontSize: 17,
+                                                              fontWeight: FontWeight
+                                                                  .bold)),
+                                                    ),
+                                                    Row(
+                                                      children: [
+                                                        Padding(
+                                                          padding: const EdgeInsets
+                                                              .all(8.0),
+                                                          child: Text(
+                                                              resultData?[index]['date'] ??
+                                                                  'None'),
+                                                        ),
+                                                        Padding(
+                                                          padding: const EdgeInsets
+                                                              .all(8.0),
+                                                          child: Text(
+                                                              resultData?[index]['time'] ??
+                                                                  'None'),
+                                                        ),
+                                                      ],
+                                                    ),
+                                                  ],
+                                                ),
+                                              ],
+                                            )
+                                        ),
+                                        Container(
+                                          padding: EdgeInsets.only(right: 10),
+                                          child: Icon(
+                                            Icons.delete,
+                                            color: Colors.red,
+                                          ),
+                                        ),
+                                      ],
+                                    )
+                                )
+                            ),
+                          ),
+                        ),
+                      ]
+                  );
+              }
+            );
+          }
         }
       );
   }
