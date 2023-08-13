711e51d67c1a04837918043dab7f297a08e3a429
sig03 <sig03@naver.com>
2023-08-14 08:43:52 +0900

    add alertdialog

--- lib/screens/schedule_list.dart
+++ lib/screens/schedule_list.dart
@@ -87,10 +87,30 @@ class _ScheduleListState extends State<ScheduleList> {
                                           child: IconButton(
                                             icon: const Icon(Icons.delete),
                                             color: Colors.red,
-                                            onPressed: () {
-                                              _delData(resultData?[index]['id'] ??
-                                                  'None');
-                                            },
+                                            // onPressed: () {
+                                            //   _delData(resultData?[index]['id'] ??
+                                            //       'None');
+                                            // },
+                                            onPressed: () => showDialog(
+                                                context: context,
+                                                builder: (BuildContext context) => AlertDialog(
+                                                  title: const Text('Delete Item'),
+                                                  content: const Text('Do you want a delete?'),
+                                                  actions: [
+                                                    TextButton(
+                                                        onPressed: () => Navigator.pop(context, 'Cancel'),
+                                                        child: const Text('Cancel')
+                                                    ),
+                                                    TextButton(
+                                                        onPressed: () {
+                                                            _delData(resultData?[index]['id'] ?? 'None');
+                                                            Navigator.pop(context);
+                                                          },
+                                                        child: const Text('Delete')
+                                                    ),
+                                                  ],
+                                                ),
+                                            ),
                                           ),
                                         ),
                                       ],
