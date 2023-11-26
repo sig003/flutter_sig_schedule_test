a81cc54819c3657dfa8bbf4a1f1f875b2dbd6977
sig03 <sig03@naver.com>
2023-11-26 17:50:43 +0900

    수정 다이얼로그 textfield 추가

--- lib/screens/modify_job_dialog.dart
+++ lib/screens/modify_job_dialog.dart
@@ -1,24 +1,147 @@
 import 'package:flutter/material.dart';
 import 'package:sig_schedule_test/screens/library.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:intl/intl.dart';
 
 Future<void> ModifyJobDialog(BuildContext context, id) async {
+  final _formKey = GlobalKey<FormState>();
+
+  TextEditingController jobInput = TextEditingController();
+  TextEditingController dateInput = TextEditingController();
+  TextEditingController timeInput = TextEditingController();
+  DateTime _selectedDate = DateTime.now();
+  DateTime _selectedTime = DateTime.now();
+
   var result = await getSharedPreference(id);
+
+  jobInput.text = result[0]['job'];
+  dateInput.text = result[0]['date'];
+  timeInput.text = result[0]['time'];
+
+  void _showDialog(Widget child) {
+    showCupertinoModalPopup<void>(
+      context: context,
+      builder: (BuildContext context) => Container(
+        height: 216,
+        padding: const EdgeInsets.only(top: 6.0),
+        margin: EdgeInsets.only(
+          bottom: MediaQuery.of(context).viewInsets.bottom,
+        ),
+        color: CupertinoColors.systemBackground.resolveFrom(context),
+        child: SafeArea(
+          top: false,
+          child: child,
+        ),
+      ),
+    );
+  }
 
   return showDialog<void>(
     context: context,
     builder: (BuildContext context) => AlertDialog(
       title: const Text('Modify Job'),
-      content: Text(result[0]['job']),
+      content: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextFormField(
+              controller: jobInput,
+              autovalidateMode:AutovalidateMode.onUserInteraction,
+              decoration: const InputDecoration(
+                border: UnderlineInputBorder(),
+                labelText: 'Job',
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+                controller: dateInput,
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                decoration: const InputDecoration(
+                  border: UnderlineInputBorder(),
+                  labelText: 'Date',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+                readOnly: true,
+                onTap: () async {
+                  DateTime? pickedDate = await showDatePicker(
+                      context: context,
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime(2000),
+                      lastDate: DateTime(2101)
+                  );
+
+                  if (pickedDate != null ) {
+                    String formattedDate = DateFormat('yyyy-MM-dd').format(pickedDate);
+
+                    //setState(() {
+                      _selectedDate = pickedDate;
+                      dateInput.text = formattedDate;
+                    //});
+                  } else {
+                    print("Date is not selected");
+                  }
+                }
+            ),
+            TextFormField(
+                controller: timeInput,
+                autovalidateMode:AutovalidateMode.onUserInteraction,
+                decoration: const InputDecoration(
+                  border: UnderlineInputBorder(),
+                  labelText: 'Time',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+                readOnly: true,
+                onTap: () async {
+                  _showDialog(
+                    CupertinoDatePicker(
+                      initialDateTime: DateTime.now(),
+                      mode: CupertinoDatePickerMode.time,
+                      use24hFormat: true,
+                      onDateTimeChanged: (DateTime newTime) {
+                        //setState(() {
+                          _selectedTime = newTime;
+                          String formattedTime = DateFormat('HH:mm').format(newTime);
+                          timeInput.text = formattedTime;
+                        //});
+                      },
+                    ),
+                  );
+                }
+            ),
+          ],
+        ),
+      ),
       actions: [
         TextButton(
-            onPressed: () => Navigator.pop(context, 'Cancel'),
-            child: const Text('Cancel')
+            child: const Text('Cancel'),
+            onPressed: () => Navigator.pop(context, 'Cancel')
         ),
         TextButton(
+            child: const Text('Modify'),
             onPressed: () {
+              if (!_formKey.currentState!.validate()) {
+                return;
+              }
+              var combinedTime = combinedDateTime(_selectedDate, _selectedTime);
+              //saveJob(jobInput, dateInput, timeInput, combinedTime, showNotification);
               Navigator.pop(context);
-            },
-            child: const Text('Modify')
+            }
         ),
       ],
     ),
